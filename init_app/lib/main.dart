0a545163653ec888dec242d197e74050b0a222eb
Martin Söderstrand (tdx204) <martin.soderstrand@teliacompany.com>
2020-01-27 22:26:42 +0100

    Create the foundation of my first flutter application

---
+++ init_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("My first app"),
+      backgroundColor: Colors.orange,
+    ),
+    backgroundColor: Colors.grey[400],
+  ),
+));
+
