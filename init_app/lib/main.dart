875bcc93eb39878d8cc9fac8d72d7588b6f37993
Martin Söderstrand (tdx204) <martin.soderstrand@teliacompany.com>
2020-01-29 21:38:42 +0100

    Added some proper state methods and changed some styling

--- init_app/lib/main.dart
+++ init_app/lib/main.dart
@@ -1,7 +1,92 @@
 import 'package:flutter/material.dart';
 
-import 'home/home.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+
+  MainPage({Key key}) : super(key: key);
+
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: buildMainPageAppBar(),
+      body: buildMainPageBody(),
+    );
+  }
+}
+
+Center buildMainPageBody() {
+  return Center(
+    child: Container(
+      margin: EdgeInsets.fromLTRB(0, 0, 0, 250),
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            buildCenterColumnContainer(
+              buildFormInputField("Full name"),
+            ),
+            buildCenterColumnContainer(
+                buildFormInputField("Email")
+            ),
+            buildCenterColumnContainer(
+                buildFormInputField("Meddelande")
+            ),
+            buildCenterColumnContainer(RaisedButton(
+              color: Colors.teal,
+              onPressed: () {
+                handleSendRequestOnPress();
+              },
+              child: Text(
+                "Contact Me",
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ),
+            )),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+TextFormField buildFormInputField(String inputFieldText) {
+  return TextFormField(
+      style: TextStyle(
+        fontSize: 15,
+      ),
+      decoration: InputDecoration(
+        labelText: inputFieldText
+      ));
+}
+
+Container buildCenterColumnContainer(Widget widget) {
+  return Container(
+      height: 50, width: 260, margin: EdgeInsets.all(10), child: widget);
+}
+
+void handleSendRequestOnPress() {
+  print("Contact Me");
+}
+
+AppBar buildMainPageAppBar() {
+  return AppBar(
+    title: Text("Flutter state management"),
+    backgroundColor: Colors.teal,
+    centerTitle: true,
+  );
+}
--- init_app/test/widget_test.dart
+++ init_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:init_app/main.dart';
+import 'package:contact/main.dart';
 
 void main() {
 
